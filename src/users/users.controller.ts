70554e289006ec607305274f9c2bc3af6abcebc0
Михаил Лейбук-Галин <michael@onepointcollab.com>
2024-09-09 19:19:12 +0300

    Fixed User Service - add logic for Exeptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,20 +2,20 @@ import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.model';
 
-@Controller('users')
+@Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post('add-user')
   createUser(
     @Body('name') name: string,
     @Body('email') email: string,
-    @Body('password') password: string,
+    @Body('phone') phone: string,
   ): Promise<User> {
-    return this.usersService.createUser(name, email, password);
+    return this.usersService.createUser(name, email, phone);
   }
 
-  @Get(':id')
+  @Get('get-user/:id')
   findById(@Param('id') id: number): Promise<User> {
     return this.usersService.findById(id);
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, BadRequestException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { User } from './user.model';
 
@@ -11,6 +11,17 @@ export class UsersService {
   }
 
   async findById(id: number): Promise<User> {
-    return this.userRepository.findById(id);
+    if (isNaN(id) || id <= 0) {
+      throw new BadRequestException(`Invalid ID: ${id}`);
+    }
+
+    const
+      user = await this.userRepository.findById(id);
+
+    if (!user) {
+      throw new BadRequestException(`User with ID ${id} not found`);
+    }
+
+    return user
   }
 }
