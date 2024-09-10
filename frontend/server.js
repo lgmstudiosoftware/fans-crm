98d001b00d659f6a44cbdc1e7b47ee9d8e1895a8
Михаил Лейбук-Галин <michael@onepointcollab.com>
2024-09-10 11:52:40 +0300

    Change port on server and client from 5173 to 3001

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,7 @@ import { Transform } from 'node:stream'
 
 // Constants
 const isProduction = process.env.NODE_ENV === 'production'
-const port = process.env.PORT || 5173
+const port = process.env.PORT || 3001
 const base = process.env.BASE || '/'
 const ABORT_DELAY = 10000
 
