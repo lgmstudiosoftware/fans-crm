3a106d68ad39e202089f2ed26fbd4154df7e1b52
lgmstudiosoftware <lgmstudiosoftware@gmail.com>
2024-09-10 22:12:07 +0300

    Fixed Bugs

--- frontend/craco.config.ts
+++ frontend/craco.config.ts
@@ -1,5 +1,21 @@
-const cracoConfig = {
-  eslint: true,
+const config = {
+  webpack: (config: any, env: any, helpers: any) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    });
+    return config;
+  },
+  devServer: (devServerConfig: any, argv: any) => {
+    devServerConfig.historyApiFallback = true;
+    return devServerConfig;
+  }
 };
 
-export default cracoConfig;
+export default config;
